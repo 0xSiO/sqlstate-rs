1baab33936844c854aa073a17a25a355c1f4376e
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-09-14 17:45:14 -0700

    Use new subclass-parsing logic for SqlState parsing

--- sqlstate/src/lib.rs
+++ sqlstate/src/lib.rs
@@ -2,7 +2,7 @@ pub mod error;
 pub mod postgres;
 pub mod subclass;
 
-// use std::{convert::TryFrom, str::FromStr};
+use std::{convert::TryFrom, str::FromStr};
 
 pub use self::{error::ParseError, subclass::*};
 
@@ -82,120 +82,88 @@ pub enum SqlState {
     Other(String),
 }
 
-// impl FromStr for SqlState {
-//     type Err = ParseError;
+impl FromStr for SqlState {
+    type Err = ParseError;
 
-//     fn from_str(value: &str) -> Result<Self, Self::Err> {
-//         // SQL standard requires length to be 5 bytes
-//         if value.len() != 5 {
-//             return Err(ParseError::InvalidLength(value.len()));
-//         }
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        // SQL standard requires length to be 5 bytes
+        if value.len() != 5 {
+            return Err(ParseError::InvalidLength(value.len()));
+        }
 
-//         let (class, subclass) = value.split_at(2);
+        let (class, subclass) = value.split_at(2);
 
-//         match class {
-//             "00" => Ok(Self::Success),
-//             "01" => Ok(Self::Warning(subclass.parse().unwrap())),
-//             "02" => Ok(Self::NoData(subclass.parse().unwrap())),
-//             "07" => Ok(Self::DynamicSqlError(DynamicSqlError::try_from(subclass)?)),
-//             "08" => Ok(Self::ConnectionException(ConnectionException::try_from(
-//                 subclass,
-//             )?)),
-//             "09" => Ok(Self::TriggeredActionException),
-//             "0A" => Ok(Self::FeatureNotSupported(FeatureNotSupported::try_from(
-//                 subclass,
-//             )?)),
-//             "0D" => Ok(Self::InvalidTargetTypeSpecification),
-//             "0E" => Ok(Self::InvalidSchemaNameListSpecification),
-//             "0F" => Ok(Self::LocatorException(LocatorException::try_from(
-//                 subclass,
-//             )?)),
-//             "0K" => Ok(Self::ResignalWhenHandlerNotActive),
-//             "0L" => Ok(Self::InvalidGrantor),
-//             "0M" => Ok(Self::InvalidSqlInvokedProcedureReference),
-//             "0N" => Ok(Self::SqlXmlMappingError(SqlXmlMappingError::try_from(
-//                 subclass,
-//             )?)),
-//             "0P" => Ok(Self::InvalidRoleSpecification),
-//             "0S" => Ok(Self::InvalidTransformGroupNameSpecification),
-//             "0T" => Ok(Self::TargetTableDisagreesWithCursorSpecification),
-//             "0U" => Ok(Self::AttemptToAssignToNonUpdatableColumn),
-//             "0V" => Ok(Self::AttemptToAssignToOrderingColumn),
-//             "0W" => Ok(Self::ProhibitedStatementDuringTriggerExecution(
-//                 ProhibitedStatementDuringTriggerExecution::try_from(subclass)?,
-//             )),
-//             "0X" => Ok(Self::InvalidForeignServerSpecification),
-//             "0Y" => Ok(Self::PassthroughSpecificCondition(
-//                 PassthroughSpecificCondition::try_from(subclass)?,
-//             )),
-//             "0Z" => Ok(Self::DiagnosticsException(DiagnosticsException::try_from(
-//                 subclass,
-//             )?)),
-//             "10" => Ok(Self::XQueryError),
-//             "20" => Ok(Self::CaseNotFoundForCaseStatement),
-//             "21" => Ok(Self::CardinalityViolation),
-//             "22" => Ok(Self::DataException(DataException::try_from(subclass)?)),
-//             "23" => Ok(Self::IntegrityConstraintViolation(
-//                 IntegrityConstraintViolation::try_from(subclass)?,
-//             )),
-//             "24" => Ok(Self::InvalidCursorState),
-//             "25" => Ok(Self::InvalidTransactionState(
-//                 InvalidTransactionState::try_from(subclass)?,
-//             )),
-//             "26" => Ok(Self::InvalidSqlStatementName),
-//             "27" => Ok(Self::TriggeredDataChangeViolation(
-//                 TriggeredDataChangeViolation::try_from(subclass)?,
-//             )),
-//             "28" => Ok(Self::InvalidAuthorizationSpecification),
-//             "2B" => Ok(Self::DependentPrivilegeDescriptorsExist),
-//             "2C" => Ok(Self::InvalidCharsetName),
-//             "2D" => Ok(Self::InvalidTransactionTermination),
-//             "2E" => Ok(Self::InvalidConnectionName),
-//             "2F" => Ok(Self::SqlRoutineException(SqlRoutineException::try_from(
-//                 subclass,
-//             )?)),
-//             "2H" => Ok(Self::InvalidCollationName),
-//             "30" => Ok(Self::InvalidSqlStatementIdentifier),
-//             "33" => Ok(Self::InvalidSqlDescriptorName),
-//             "34" => Ok(Self::InvalidCursorName),
-//             "35" => Ok(Self::InvalidConditionNumber),
-//             "36" => Ok(Self::CursorSensitivityException(
-//                 CursorSensitivityException::try_from(subclass)?,
-//             )),
-//             "38" => Ok(Self::ExternalRoutineException(
-//                 ExternalRoutineException::try_from(subclass)?,
-//             )),
-//             "39" => Ok(Self::ExternalRoutineInvocationException(
-//                 ExternalRoutineInvocationException::try_from(subclass)?,
-//             )),
-//             "3B" => Ok(Self::SavepointException(SavepointException::try_from(
-//                 subclass,
-//             )?)),
-//             "3C" => Ok(Self::AmbiguousCursorName),
-//             "3D" => Ok(Self::InvalidCatalogName),
-//             "3F" => Ok(Self::InvalidSchemaName),
-//             "40" => Ok(Self::TransactionRollback(TransactionRollback::try_from(
-//                 subclass,
-//             )?)),
-//             "42" => Ok(Self::SyntaxErrorOrAccessRuleViolation),
-//             "44" => Ok(Self::WithCheckOptionViolation),
-//             "45" => Ok(Self::UnhandledUserDefinedException),
-//             "46" => Ok(Self::OlbSpecificError(OlbSpecificError::try_from(
-//                 subclass,
-//             )?)),
-//             "HW" => Ok(Self::DatalinkException(DatalinkException::try_from(
-//                 subclass,
-//             )?)),
-//             "HV" => Ok(Self::FdwSpecificCondition(FdwSpecificCondition::try_from(
-//                 subclass,
-//             )?)),
-//             "HY" => Ok(Self::CliSpecificCondition(CliSpecificCondition::try_from(
-//                 subclass,
-//             )?)),
-//             "HZ" => Ok(Self::RemoteDatabaseAccess(RemoteDatabaseAccess::try_from(
-//                 subclass,
-//             )?)),
-//             other => Ok(Self::Other(value.to_string())),
-//         }
-//     }
-// }
+        match class {
+            "00" => Ok(Self::Success),
+            "01" => Ok(Self::Warning(subclass.parse().unwrap())),
+            "02" => Ok(Self::NoData(subclass.parse().unwrap())),
+            "07" => Ok(Self::DynamicSqlError(subclass.parse().unwrap())),
+            "08" => Ok(Self::ConnectionException(subclass.parse().unwrap())),
+            "09" => Ok(Self::TriggeredActionException),
+            "0A" => Ok(Self::FeatureNotSupported(subclass.parse().unwrap())),
+            "0D" => Ok(Self::InvalidTargetTypeSpecification),
+            "0E" => Ok(Self::InvalidSchemaNameListSpecification),
+            "0F" => Ok(Self::LocatorException(subclass.parse().unwrap())),
+            "0K" => Ok(Self::ResignalWhenHandlerNotActive),
+            "0L" => Ok(Self::InvalidGrantor),
+            "0M" => Ok(Self::InvalidSqlInvokedProcedureReference),
+            "0N" => Ok(Self::SqlXmlMappingError(subclass.parse().unwrap())),
+            "0P" => Ok(Self::InvalidRoleSpecification),
+            "0S" => Ok(Self::InvalidTransformGroupNameSpecification),
+            "0T" => Ok(Self::TargetTableDisagreesWithCursorSpecification),
+            "0U" => Ok(Self::AttemptToAssignToNonUpdatableColumn),
+            "0V" => Ok(Self::AttemptToAssignToOrderingColumn),
+            "0W" => Ok(Self::ProhibitedStatementDuringTriggerExecution(
+                subclass.parse().unwrap(),
+            )),
+            "0X" => Ok(Self::InvalidForeignServerSpecification),
+            "0Y" => Ok(Self::PassthroughSpecificCondition(
+                subclass.parse().unwrap(),
+            )),
+            "0Z" => Ok(Self::DiagnosticsException(subclass.parse().unwrap())),
+            "10" => Ok(Self::XQueryError),
+            "20" => Ok(Self::CaseNotFoundForCaseStatement),
+            "21" => Ok(Self::CardinalityViolation),
+            "22" => Ok(Self::DataException(subclass.parse().unwrap())),
+            "23" => Ok(Self::IntegrityConstraintViolation(
+                subclass.parse().unwrap(),
+            )),
+            "24" => Ok(Self::InvalidCursorState),
+            "25" => Ok(Self::InvalidTransactionState(subclass.parse().unwrap())),
+            "26" => Ok(Self::InvalidSqlStatementName),
+            "27" => Ok(Self::TriggeredDataChangeViolation(
+                subclass.parse().unwrap(),
+            )),
+            "28" => Ok(Self::InvalidAuthorizationSpecification),
+            "2B" => Ok(Self::DependentPrivilegeDescriptorsExist),
+            "2C" => Ok(Self::InvalidCharsetName),
+            "2D" => Ok(Self::InvalidTransactionTermination),
+            "2E" => Ok(Self::InvalidConnectionName),
+            "2F" => Ok(Self::SqlRoutineException(subclass.parse().unwrap())),
+            "2H" => Ok(Self::InvalidCollationName),
+            "30" => Ok(Self::InvalidSqlStatementIdentifier),
+            "33" => Ok(Self::InvalidSqlDescriptorName),
+            "34" => Ok(Self::InvalidCursorName),
+            "35" => Ok(Self::InvalidConditionNumber),
+            "36" => Ok(Self::CursorSensitivityException(subclass.parse().unwrap())),
+            "38" => Ok(Self::ExternalRoutineException(subclass.parse().unwrap())),
+            "39" => Ok(Self::ExternalRoutineInvocationException(
+                subclass.parse().unwrap(),
+            )),
+            "3B" => Ok(Self::SavepointException(subclass.parse().unwrap())),
+            "3C" => Ok(Self::AmbiguousCursorName),
+            "3D" => Ok(Self::InvalidCatalogName),
+            "3F" => Ok(Self::InvalidSchemaName),
+            "40" => Ok(Self::TransactionRollback(subclass.parse().unwrap())),
+            "42" => Ok(Self::SyntaxErrorOrAccessRuleViolation),
+            "44" => Ok(Self::WithCheckOptionViolation),
+            "45" => Ok(Self::UnhandledUserDefinedException),
+            "46" => Ok(Self::OlbSpecificError(subclass.parse().unwrap())),
+            "HW" => Ok(Self::DatalinkException(subclass.parse().unwrap())),
+            "HV" => Ok(Self::FdwSpecificCondition(subclass.parse().unwrap())),
+            "HY" => Ok(Self::CliSpecificCondition(subclass.parse().unwrap())),
+            "HZ" => Ok(Self::RemoteDatabaseAccess(subclass.parse().unwrap())),
+            _ => Ok(Self::Other(value.to_string())),
+        }
+    }
+}
