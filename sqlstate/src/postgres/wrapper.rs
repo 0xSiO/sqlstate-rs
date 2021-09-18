5f3f927287d55e2da68dab66295a8a2ba11e51a1
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-09-17 17:21:58 -0700

    Fix error handling for PostgresSqlState parsing

--- sqlstate/src/postgres/wrapper.rs
+++ sqlstate/src/postgres/wrapper.rs
@@ -1,6 +1,6 @@
-use std::{convert::Infallible, str::FromStr};
+use std::str::FromStr;
 
-use crate::{postgres, standard, Category};
+use crate::{error::ParseError, postgres, standard, Category};
 
 #[cfg_attr(docsrs, doc(cfg(feature = "postgres")))]
 #[derive(Clone, Eq, PartialEq, Hash, Debug)]
@@ -19,10 +19,12 @@ impl PostgresSqlState {
 }
 
 impl FromStr for PostgresSqlState {
-    type Err = Infallible;
+    type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(s.parse::<postgres::SqlState>()
-            .map_or_else(|_| Self::Standard(s.parse().unwrap()), Self::Custom))
+        match s.parse::<postgres::SqlState>() {
+            Ok(state) => Ok(Self::Custom(state)),
+            Err(_) => Ok(Self::Standard(s.parse()?)),
+        }
     }
 }
