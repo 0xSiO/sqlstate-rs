5ed33eb8310a7a6c7a49cbe3600b2ab47c867ad1
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-09-18 16:37:00 -0700

    Add crate docs, re-export standard::SqlState

--- sqlstate/src/lib.rs
+++ sqlstate/src/lib.rs
@@ -1,5 +1,26 @@
 #![warn(rust_2018_idioms)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+//! Representations and parsing logic for [`SQLSTATE`](https://en.wikipedia.org/wiki/SQLSTATE)
+//! return codes.
+//!
+//! # Example
+//!
+//! ```
+//! use sqlstate::{
+//!    standard::{
+//!        class::{DataException::DivisionByZero, Warning::PrivilegeNotGranted},
+//!        SqlState,
+//!    },
+//! };
+//!
+//! # fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! assert_eq!("00000".parse::<SqlState>()?, SqlState::Success(None));
+//! assert_eq!("01007".parse::<SqlState>()?, SqlState::Warning(Some(PrivilegeNotGranted)));
+//! assert_eq!("22012".parse::<SqlState>()?, SqlState::DataException(Some(DivisionByZero)));
+//! assert_eq!("XX102".parse::<SqlState>()?, SqlState::Other(String::from("XX102")));
+//! #     Ok(())
+//! # }
+//! ```
 
 pub mod error;
 pub mod standard;
@@ -10,6 +31,8 @@ pub mod postgres;
 
 #[cfg(feature = "postgres")]
 pub use postgres::wrapper::PostgresSqlState;
+
+pub use standard::SqlState;
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub enum Category {
